92c1e2737709264e53b79763cd2c5b17aff7fe70
Eric Templin <ertemplin@gmail.com>
2012-02-23 02:05:09 -0500

    Remove name from console

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Change etemplin to whoever you want to annoy.
+# Also, change tty7 to whatever their terminal is.
+# echo -e '\a' | write etemplin tty6
+
+# prints out "usernames computer"
+# who | grep tty7 | awk '{print $1}' | xargs -I {} echo {}s computer
+
+NAME=`who | grep tty7 | awk '{print $1}' | xargs echo -n`
+TTY=tty7
+echo $TTY
+echo $NAME
+echo -e '\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n ' | write $NAME $TTY
