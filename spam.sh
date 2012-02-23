d4e9cc0b27790ad73c776d7b20108f90d730f0b9
Eric Templin <ertemplin@gmail.com>
2012-02-22 23:00:13 -0500

    spam.sh will loop through every computer in Laswon

---
+++ spam.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# 
+# Loops through every linux computer in Lawson
+#
+# For more fun, try: for i in {1..10}; do ./spam.sh; done;
+
+
+
+function doRing {
+	echo '/homes/etemplin/testing/bell/ring.sh' | ssh -o StrictHostKeyChecking=no etemplin@$1.cs.purdue.edu 2>&1 >/dev/null
+}
+
+ME=$$
+
+for host in moore sslab; do
+	for id in {00..24}; do
+		doRing ${host}${id} &
+	done
+done
+
+for i in {1..5}; do
+	for j in {1..5}; do
+		doRing pod${i}-${j} &
+	done
+done
+
+for id in {01..13}; do
+	doRing sac${id} &
+done
+
+sleep 2
+kill -9 $ME
