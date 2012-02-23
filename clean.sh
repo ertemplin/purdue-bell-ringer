01273e800e8c63d43297423851abac658948a97c
Eric Templin <ertemplin@gmail.com>
2012-02-23 12:59:41 -0500

    Cleaned up comments.

--- clean.sh
+++ clean.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-# Change etemplin to whoever you want to annoy.
-# Also, change tty7 to whatever their terminal is.
-# echo -e '\a' | write etemplin tty6
-
-# prints out "usernames computer"
-# who | grep tty7 | awk '{print $1}' | xargs -I {} echo {}s computer
+# Replaces "Message recieved from..." with newlines
 
 NAME=`who | grep tty7 | awk '{print $1}' | xargs echo -n`
 TTY=tty7
