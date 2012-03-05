a28afff336f2234418e345ad910016dc1297529b
Eric Templin <ertemplin@gmail.com>
2012-03-05 12:31:03 -0500

    May have fixed multiple user problem. Not sure.

--- spam.sh
+++ spam.sh
@@ -5,19 +5,20 @@
 # For more fun, try: for i in {1..10}; do ./spam.sh; done;
 
 NAME=`whoami`
+RINGER_PATH=$PWD"/ring.sh\n"
 
 function doRing {
 	# TODO Change this line here to match the path of the ring.sh script before running
-	echo '/homes/etemplin/testing/bell/ring.sh' | ssh -o StrictHostKeyChecking=no $NAME@$1.cs.purdue.edu 2>&1 >/dev/null
+	$RINGER_PATH | ssh -o StrictHostKeyChecking=no $NAME@$1.cs.purdue.edu 2>&1 >/dev/null
 }
 
 ME=$$
 
-for host in moore sslab; do
-	for id in {00..24}; do
-		doRing ${host}${id} &
-	done
-done
+# for host in moore sslab; do
+# 	for id in {00..24}; do
+# 		doRing ${host}${id} &
+# 	done
+# done
 
 for i in {1..5}; do
 	for j in {1..5}; do
@@ -25,9 +26,9 @@ for i in {1..5}; do
 	done
 done
 
-for id in {01..13}; do
-	doRing sac${id} &
-done
+# for id in {01..13}; do
+# 	doRing sac${id} &
+# done
 
 sleep 2
 kill -9 $ME
