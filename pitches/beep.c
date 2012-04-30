255da7f9b870be409e03edd17f5f1e9487313469
Eric Templin <ertemplin@gmail.com>
2012-04-30 01:00:35 -0400

    Removed pitches

--- pitches/beep.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <fcntl.h>
-#include <linux/kd.h>
-
-int main(int argc, char *argv[]) {
-	int fd = open("/dev/snd/controlC0", O_RDONLY);
-	if (fd == -1 || argc != 3) return -1;
-	return ioctl(fd, KDMKTONE, (atoi(argv[2])<<16)+(1193180/atoi(argv[1])));
-}
