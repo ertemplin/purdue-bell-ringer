e755e467403b8995efa7b4a8819e290c06232949
Eric Templin <ertemplin@gmail.com>
2012-04-30 01:01:18 -0400

    Added new deploy script.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Deploying command '$@'..."
+
+for HOST in moore{00..24} sslab{00..24} xinu{00..21} sac{01..13} pod{1..5}-{1..5} pod0-0 mc{01..18} escher{00..23}; do
+    echo $HOST
+    echo "nohup $@ >/dev/null 2>&1 & exit" | ssh -o StrictHostKeyChecking=no zhangw@${HOST}.cs.purdue.edu >>/tmp/deploy_zhangw 2>&1 &
+done
+
+echo "Done."
